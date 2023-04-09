a14faa218b2dc243b870f7bc4030bddadf82eb4e
HW_Amos <446296992@qq.com>
2023-04-09 17:31:06 +0800

    Update nav.config.ts

--- nav.config.ts
+++ nav.config.ts
@@ -4,7 +4,6 @@ const c: IConfig = {
   // [Mondatory], Please replace following Github url with your own Github address which you forked in.
   // [必填], 请填写您的仓库地址
   gitRepoUrl: 'https://github.com/yqbgq/nav',
-
   
   // 部署分支
   branch: 'main',
